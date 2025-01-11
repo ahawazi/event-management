ca4ce5a5f40db9c91de00eb9eecaf4383ef158e7
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2025-01-11 15:50:07 +0330

    feat(conference): add model, factory, migration

---
+++ app/Models/Conference.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Conference extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'decisions',
+        'start_date',
+        'end_date',
+        'status',
+        'region',
+        'venue',
+        'venue_id',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'start_date' => 'datetime',
+        'end_date' => 'datetime',
+        'venue' => 'integer',
+        'venue_id' => 'integer',
+    ];
+
+    public function venue(): BelongsTo
+    {
+        return $this->belongsTo(Venue::class);
+    }
+
+    public function speakers(): BelongsToMany
+    {
+        return $this->belongsToMany(Speaker::class);
+    }
+
+    public function talks(): BelongsToMany
+    {
+        return $this->belongsToMany(Talk::class);
+    }
+}
