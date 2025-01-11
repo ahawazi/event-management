954743e03588020ef03bebcd83c2ed89dd62cb12
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2025-01-11 17:05:16 +0330

    refactor(models): remove mass assignable attributes from Conference, Speaker, Talk, and Venue models

--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -11,27 +11,6 @@ class Conference extends Model
 {
     use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name',
-        'decisions',
-        'start_date',
-        'end_date',
-        'status',
-        'region',
-        'venue',
-        'venue_id',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'id' => 'integer',
         'start_date' => 'datetime',
