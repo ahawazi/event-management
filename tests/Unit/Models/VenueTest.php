78e15112c6ea0988d1c45821fc962cf4d5c1e3bc
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2025-01-14 15:36:05 +0330

    test (venue): add unit test for Venue model attributes

---
+++ tests/Unit/Models/VenueTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Models\Venue;
+
+it('has attributes', function () {
+    expect(Venue::factory()->create())
+        ->name->not->toBeNull()
+        ->city->not->toBeNull()
+        ->country->not->toBeNull()
+        ->postal_code->not->toBeNull();
+});
