6dad6e2408d50d2c31eacc323b86291f15b701c0
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2025-01-27 17:08:05 +0330

    feat(resources): refactor ConferenceResource form to use getForm method and enhance structure

--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -3,6 +3,16 @@
 namespace App\Models;
 
 use App\Enums\Region;
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Get;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -34,5 +44,110 @@ class Conference extends Model
     public function talks(): BelongsToMany
     {
         return $this->belongsToMany(Talk::class);
+    }
+
+    public static function getForm(): array
+    {
+        return [
+            //also we can use the tab to separate the form into multiple sections
+            Section::make('Conference Details')
+                // this is like a sidebar
+                // ->aside()
+                ->collapsible()
+                ->icon('heroicon-o-information-circle')
+                ->description('Provide some basic information about the conference.')
+                ->columns(['md' => 2, 'lg' => 3])
+                ->schema([
+                    TextInput::make('name')
+                        ->columnSpanFull()
+
+                        ->label('Conference Name')
+
+                        ->helperText('The name of the conference.')
+                        // ->placeholder('Enter the name of the conference.')
+
+                        // hint is a thing in top right corner of the input
+                        // ->hint('The name of the conference.')
+                        // ->hintIcon('heroicon-o-rectangle-stack')
+                        // ->hintAction('https://google.com')
+
+                        ->required()
+                        ->maxLength(60),
+
+                    RichEditor::make('decisions')
+                        ->columnSpanFull()
+
+                        ->required()
+
+                        ->placeholder('Enter the decisions of the conference.')
+
+                        // chose what buttons do't want to show
+                        // ->disableToolbarButtons(['italic'])
+                        // chose what buttons do want to show
+                        ->ToolbarButtons(['bold', 'link', 'h1', 'h2']),
+
+                    DateTimePicker::make('start_date')
+                        // hide native date picker
+                        ->native(false)
+                        ->required(),
+
+                    DateTimePicker::make('end_date')
+                        ->native(false)
+                        ->required(),
+                    Fieldset::make('Status')
+                        ->columns(1)
+                        ->schema([
+
+                            Select::make('status')
+                                ->options([
+                                    'draft' => 'Draft',
+                                    'published' => 'Published',
+                                    'archived' => 'Archived',
+                                ])
+                                ->required(),
+                            Toggle::make('is_published')
+                                ->default(false),
+                        ])
+                ]),
+
+            Section::make('Location')
+                ->columns(2)
+                ->schema([
+                    Select::make('region')
+                        ->live()
+                        ->required()
+                        ->enum(Region::class)
+                        ->options(Region::class),
+
+                    Select::make('venue_id')
+                        ->searchable()
+                        ->preload()
+
+                        ->editOptionForm(Venue::getForm())
+                        ->createOptionForm(Venue::getForm())
+
+                        ->relationship('venue', 'name', modifyQueryUsing: function (Builder $query, Get $get) {
+                            return $query->where('region', $get('region'));
+                        }),
+                ]),
+
+
+
+            CheckboxList::make('speakers')
+                ->relationship('speakers', 'name')
+                ->options(
+                    Speaker::all()->pluck('name', 'id')
+                )
+
+
+            // TODO
+            // TextInput::make('website')
+            //     ->url()
+            //     ->label('Your Website')
+            //     ->prefix('https://')
+            //     ->prefixIcon('heroicon-o-globe-alt')
+            //     ->suffix('.com')
+            //     ->maxLength(255),
+        ];
     }
 }
