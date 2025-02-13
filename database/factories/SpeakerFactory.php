e6c0a8fd8e75f79c963127a127dd88840ac7cd55
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2025-02-13 19:35:57 +0330

    feat(speaker): enhance SpeakerFactory to include dynamic qualifications and relationship with talks

--- database/factories/SpeakerFactory.php
+++ database/factories/SpeakerFactory.php
@@ -5,6 +5,8 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use App\Models\Speaker;
+use App\Models\Talk;
+
 
 class SpeakerFactory extends Factory
 {
@@ -17,23 +19,21 @@ class SpeakerFactory extends Factory
 
     public function definition(): array
     {
+        $qualificationsCount = fake()->numberBetween(0, 10);
+        $qualifications = fake()->randomElements(array_keys(Speaker::QUALIFICATIONS), $qualificationsCount);
+
+
         return [
             'name' => fake()->name(),
             'email' => fake()->safeEmail(),
             'bio' => fake()->text(),
-            'qualifications' => fake()->randomElements([
-                'business-leader',
-                'charisma',
-                'first-time',
-                'hometown-hero',
-                'humanitrain',
-                'laracasts-contributor',
-                'twitter-influencer',
-                'youtube-influencer',
-                'open-source',
-                'unique-perspective',
-            ]),
+            'qualifications' => $qualifications,
             'twitter_handle' => fake()->word(),
         ];
+    }
+
+    public function withTalks(int $count = 1): self
+    {
+        return $this->has(Talk::factory()->count($count), 'talks');
     }
 }
