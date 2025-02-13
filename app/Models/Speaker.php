3262c2ab46348e9a872657b53f5dd1ad4133be67
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2025-02-13 19:36:02 +0330

    feat(speaker): add qualifications constant and relationship with talks feat(talks): integrate TalkLength and TalkStatus enums into Talk model

--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -9,15 +9,34 @@ use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Speaker extends Model
 {
     use HasFactory;
+    // qualifications
+    const QUALIFICATIONS = [
+        'business-leader' => 'Business Leader',
+        'charisma' => 'Charismatic Speaker',
+        'first-time' => 'First Time Speaker',
+        'hometown-hero' => 'Hometown Hero',
+        'humanitrain' => 'Works in Humanitarian Field',
+        'laracasts-contributor' => 'Laracasts Contributor',
+        'twitter-influencer' => 'large Twitter Following',
+        'youtube-influencer' => 'large Youtube Following',
+        'open-source' => 'Open Source Creator / maintainer',
+        'unique-perspective' => 'Unique Perspective',
+    ];
 
     protected $casts = [
         'id' => 'integer',
         'qualifications' => 'array',
     ];
+
+    public function talks(): HasMany
+    {
+        return $this->hasMany(Talk::class);
+    }
 
     public function conferences(): BelongsToMany
     {
@@ -51,20 +70,7 @@ class Speaker extends Model
                 ->searchable()
                 //this will allow the user to select multiple options
                 ->bulkToggleable()
-                ->options(
-                    [
-                        'business-leader' => 'Business Leader',
-                        'charisma' => 'Charismatic Speaker',
-                        'first-time' => 'First Time Speaker',
-                        'hometown-hero' => 'Hometown Hero',
-                        'humanitrain' => 'Works in Humanitarian Field',
-                        'laracasts-contributor' => 'Laracasts Contributor',
-                        'twitter-influencer' => 'large Twitter Following',
-                        'youtube-influencer' => 'large Youtube Following',
-                        'open-source' => 'Open Source Creator / maintainer',
-                        'unique-perspective' => 'Unique Perspective',
-                    ]
-                )
+                ->options(self::QUALIFICATIONS)
                 ->descriptions([
                     'business-leader' => 'This speaker is a business leader.',
                     'charisma' => 'This speaker is a charismatic speaker.',
