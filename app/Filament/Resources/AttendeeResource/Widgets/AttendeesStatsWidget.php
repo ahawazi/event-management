5cc1d19b37a9a11dbf65d6e508e5aa9ceb49f29a
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2025-02-26 18:54:46 +0330

    feat (atteendee) : add resource and header widget

---
+++ app/Filament/Resources/AttendeeResource/Widgets/AttendeesStatsWidget.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\AttendeeResource\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class AttendeesStatsWidget extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            //
+        ];
+    }
+}
